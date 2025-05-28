6befddd3e4304153659a8c1a100dad2ca004cdbb
dhidrod <adanielhidalgo@gmail.com>
2025-05-28 19:42:13 +0200

    Corregido un error de sintaxis

--- resources/views/got/houses/show.blade.php
+++ resources/views/got/houses/show.blade.php
@@ -21,7 +21,7 @@
             @endforeach
         </ul>
         <a href="{{ route('got.houses.index') }}"
-           class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+           class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
             ← Volver a casas
         </a>
     </div>

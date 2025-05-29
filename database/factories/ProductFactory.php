013dd7bb15eb88723eb9f65fb44a1774ac81b249
dhidrod <adanielhidalgo@gmail.com>
2025-05-29 20:27:19 +0200

    Actualizado el factory de producto para que tenga nombres de productos de verdad.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,8 +16,41 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $products = [
+            "Jabón líquido para manos",
+            "Detergente en polvo para ropa",
+            "Limpiador multiusos",
+            "Desinfectante de superficies",
+            "Limpiador de vidrios",
+            "Limpiador de baño",
+            "Limpiador de cocina",
+            "Esponja multiusos",
+            "Toallitas desinfectantes",
+            "Aromatizante ambiental",
+            "Suavizante de telas",
+            "Limpiador de pisos",
+            "Cepillo para inodoro",
+            "Trapeador",
+            "Escoba",
+            "Guantes de limpieza",
+            "Bayeta microfibra",
+            "Limpiador de alfombras",
+            "Quitamanchas",
+            "Limpia muebles",
+            "Jabón en barra",
+            "Limpia hornos",
+            "Limpiador de acero inoxidable",
+            "Limpiador de tapicería",
+            "Desengrasante industrial",
+            "Limpiaparabrisas",
+            "Cera para pisos",
+            "Limpiador de madera",
+            "Limpiador de cuero",
+            "Abrillantador de metales"
+        ];
+
         return [
-            'name'        => $this->faker->words(3, true),
+            'name'        => $this->faker->randomElement($products),
             'description' => $this->faker->sentence(),
             'price'       => $this->faker->randomFloat(2, 1, 100),
         ];
